ed802eb7bcb52ebbba9d621088abacdfe3738d0d
njrcy <156408182+njrcy@users.noreply.github.com>
2024-02-23 08:29:24 +0000

    week 7

--- main.c
+++ main.c
@@ -7,23 +7,39 @@
 
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
+  NodePtr tailPtr=NULL;
+   int x;
+// 5 x 9 x 7 x 8 2   
+
+enqueue(&headPtr,&tailPtr,5);
+x = dequeue(&headPtr,&tailPtr);
+
+
+//For struct Queue
   Queue  q;
-   q. headPtr=NULL;
+   q.headPtr=NULL;
    q.tailPtr=NULL;
    q.size=0;
-*/
-   int i,x;
+  // enqueue_struct(&q,7);
+  // printf("%d\n",dequeue_struct(&q));
+  // printf("%d\n",dequeue_struct(&q));
+  // enqueue_struct(&q,8);
+  // printf("%d\n",dequeue_struct(&q));
+
+   int i;
    
 
  for(i=1;i<argc;i++){
+
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
+          if(q.size>=1){
+            x = dequeue_struct(&q);
             printf("dequeing %d\n",x);
+          }
+          else printf("Empty queue\n");
         }
         else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+       enqueue_struct(&q, atoi(argv[i]));
            
         }
  }
